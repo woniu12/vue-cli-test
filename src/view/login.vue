e5ab7da2cbf9e2d256efbd227ff317d56eb77968
syl <yonglei.shang@ikang.com>
2019-04-09 17:52:30 +0800

    fix

--- src/view/login.vue
+++ src/view/login.vue
@@ -8,8 +8,18 @@
 import Cookie from 'js-cookie'
 export default {
   name: 'login',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    loginStatus () {
+      return this.$store.state.login.loginStatus
+    }
+  },
   methods: {
     login () {
+      console.log(this.loginStatus)
       this.$http.post('http://api.school.cc/login', {
         username: '',
         password: ''
